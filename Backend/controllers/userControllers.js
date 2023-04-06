35e8e4c34fb4b31ba13d7445bcaa1c655374ca4b
Nguyen Van Tai <nv.tai2304@gmail.com>
2023-04-06 14:25:18 +0700

    done

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -11,9 +11,15 @@ const userController = {
   },
   deleteUsers: async (req, res) => {
     try {
-      const user = await User.findByIdAndDelete(req.params.id);
-      if (user) return res.status(200).json({ message: "Delete success." });
-      return res.status(200).json({ message: "Invail id username." });
+      if (req.user?.admin === true || req.user?.id === req.params.id) {
+        const userDelate = await User.findByIdAndDelete(req.params.id);
+        if (userDelate) {
+          return res.status(200).json({ message: "Delete success." });
+        }
+        return res.status(200).json({ message: "Invail id username." });
+      } else {
+        return res.status(200).json({ message: "Delete Fail." });
+      }
     } catch (erro) {
       return res.status(500).json(erro);
     }
