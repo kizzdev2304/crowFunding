fbc9935f69e92840004095f6ee9dfbaf1193bcd8
Nguyen Van Tai <nv.tai2304@gmail.com>
2023-04-13 23:22:32 +0700

    deloy

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-
+import React from 'react'
 const App = () => {
-  return <div className="top-1"></div>;
-};
+  return <div className='top-1'></div>
+}
 
-export default App;
+export default App
--- Frontend/src/store/index.ts
+++ Frontend/src/store/index.ts
@@ -1,42 +1,8 @@
-import { applyMiddleware, compose, createStore, StoreEnhancer } from 'redux';
-import type { Middleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
-import rootReducer from './rootReducer';
-import rootSaga from './rootSaga';
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const configureStore = (preloadedState: RootState = {} as RootState) => {
-  const sagaMiddleware = createSagaMiddleware();
-  const middlewares: Middleware[] = [sagaMiddleware];
-
-  if (import.meta.env.MODE === 'development') {
-    middlewares.push(logger);
-  }
-
-  const composeEnhancers =
-    window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const middlewareEnhancer = applyMiddleware(...middlewares);
-  // Add enhancers here
-  const enhancers = [middlewareEnhancer];
-  const composedEnhancers: StoreEnhancer = composeEnhancers(...enhancers);
-
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
-
-  sagaMiddleware.run(rootSaga);
-
-  return store;
-};
-
-// Preloaded state is used to initialize the store with data possibly from localStorage
-const preloadedState = {} as RootState;
-const store = configureStore(preloadedState);
-
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch;
-export default store;
+import { configureStore } from '@reduxjs/toolkit'
+import logger from 'redux-logger'
+const store = configureStore({
+  reducer: {},
+  middleware: (gDM) => gDM().concat(logger)
+})
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispath = typeof store.dispatch
