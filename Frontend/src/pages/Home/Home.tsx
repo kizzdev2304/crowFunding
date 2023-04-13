305c16792594752ba6b3ef117018e85e6f3c6c3e
Nguyen Van Tai <nv.tai2304@gmail.com>
2023-04-13 19:48:38 +0700

    boilerplate

---
+++ Frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Login from '@/pages/Login';
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Login />
+    </div>
+  );
+};
+
+export default Home;
